512636eb50025747b27135b70f6ca9bd3028f61d
Xavier Navarro <xanasa14@hotmail.com>
2019-04-03 23:41:04 -0400

    Update TestNGFeatures.java

--- TestNGFeatures.java
+++ TestNGFeatures.java
@@ -8,6 +8,16 @@ public void loginTest() {
 }
 	
 //invocationCount = 10 will run 10 times that particular test case. It has to be in between parenthesis right next to the annotations @Test 	
+
+//invocationTimeOut=2000 will time out the test case in 2 seconds. The number goes in milliseconds.
+	
+// expectedException= NumberFormatException.class to run a error and succeed it 
+@Test (expectedException=NumberFormatException.class)
+public void test1(){
+	String x = "100A";
+	Integer.parseInt(x);
+}
+	
 @Test (dependsOnMethods = "loginTest")
 public void homePageTest() {
 	System.out.println("homePage Test");
