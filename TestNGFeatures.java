7c5aa2d568cc3b98800427764b53794e09d96dcd
Xavier Navarro <xanasa14@hotmail.com>
2019-04-03 23:33:11 -0400

    Update TestNGFeatures.java

--- TestNGFeatures.java
+++ TestNGFeatures.java
@@ -6,6 +6,8 @@ public void loginTest() {
 	System.out.println("login Test");
 	int i = 9/0;
 }
+	
+//invocationCount = 10 will run 10 times that particular test case. It has to be in between parenthesis right next to the annotations @Test 	
 @Test (dependsOnMethods = "loginTest")
 public void homePageTest() {
 	System.out.println("homePage Test");
