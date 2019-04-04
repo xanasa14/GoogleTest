2123e4b0764220b1747775da78c922f46a61c1d8
Xavier Navarro <xanasa14@hotmail.com>
2019-04-03 23:46:28 -0400

    Update GoogleTest.java

--- GoogleTest.java
+++ GoogleTest.java
@@ -2,6 +2,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,7 +24,8 @@ public class GoogleTest {
 	@Test (groups = "Title")
 	public void googleTitleTest() {
 		String title = driver.getTitle();
-		System.out.println(title);
+		//					actual, expected, in case it fails print this 
+		Assert.assertEquals(title, "Google", "This is not a matched");
 	}
 	@Test (groups = "Logo")
 	public void googleLogoTest() {
